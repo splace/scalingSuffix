d2ad3dfc8e56ee56cf2e717e5032817fd3950660
splace <psiplace@netscape.net>
2025-05-25 17:01:31 +0100

    dims stringer

--- units.go
+++ units.go
@@ -2,6 +2,9 @@ package scalingSuffix
 
 import "golang.org/x/exp/constraints"
 import "fmt"
+import "strconv"
+import "strings"
+import "slices"
 
 // SIUnits's are SI's with metric units suffixes added.
 type SIUnits[N constraints.Float | constraints.Integer] struct{SI[N];fmt.Stringer}
@@ -17,6 +20,34 @@ func NewSIUnits[N constraints.Float | constraints.Integer](v N, f fmt.Stringer) 
 //	Masss
 //	Time
 //)
+
+func DimString(d int8, s string)(_ string){
+	switch d{
+	case 0:
+		return
+	case 1:
+		return s
+	default:
+		return s+strconv.Itoa(int(d))
+	}
+}
+
+type Dims struct{M,L,T int8}
+
+func (d Dims) String()string{
+	return strings.Join(
+		slices.DeleteFunc(
+			[]string{
+				DimString(d.M,"kg"),
+				DimString(d.L,"m"),
+				DimString(d.T,"s"),
+			},
+			func(s string)bool{return s==""},
+		),
+		".",
+	)
+}
+
 
 //type BaseUnit uint8
 
