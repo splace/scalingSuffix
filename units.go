cd6ac11d9ff87745b9d7a2c1861713c3f65d521c
splace <psiplace@netscape.net>
2025-05-27 00:51:30 +0100

    extend/scaled

--- units.go
+++ units.go
@@ -2,9 +2,11 @@ package scalingSuffix
 
 //import "golang.org/x/exp/constraints"
 import "fmt"
+import "math"
 import "strconv"
 import "strings"
 import "slices"
+
 
 // SIUnits's are SI's with metric units suffixes added.
 type SIUnits[N Number] struct{SI[N];fmt.Stringer}
@@ -14,54 +16,64 @@ func NewSIUnits[N Number](v N, f fmt.Stringer) SIUnits[N]{
 }
 
 var (
-	Mass = Dims{1,0,0,0,0,0,0}
-	Length = Dims{0,1,0,0,0,0,0}
-	Time = Dims{0,0,1,0,0,0,0}
-	Ampere  = Dims{0,0,0,1,0,0,0}
-	Kelvin  = Dims{0,0,0,0,1,0,0}
-	Mole 	 = Dims{0,0,0,0,0,1,0}
-	Candela = Dims{0,0,0,0,0,0,1}
+	Mass = Dims{M:1}
+	Length = Dims{L:1}
+	Time = Dims{T:1}
+	Ampere  = Dims{a:1}
+	Kelvin  = Dims{k:1}
+	Mole 	 = Dims{m:1}
+	Candela = Dims{c:1}
 )
 
 var (
-	Energy = Derived{1,2,-2,0,0,0,0}
-	Force = Derived{1,1,-2,0,,0,0}
-	Power = Derived{1,2,-3,0,0,0,0}
-	Pressure = Derived{1,-1,-2,0,0,0,0}
-	Frequency =	Derived{0,0,-1,0,0,0,0}
-	Coulomb = Derived{0,0,1,1,0,0,0} // C 	electric charge 	s⋅A 	
-	Volt= Derived{1,2,-3,-1,0,0,0} // 	V 	electric potential difference[a] 	kg⋅m2⋅s−3⋅A−1 	W/A = J/C
-	Farad= Derived{-1,-2,4,2,0,0,0} // F 	capacitance 	kg−1⋅m−2⋅s4⋅A2 	C/V = C2/J
-	Ohm= Derived{1,2,-3,-2,0,0,0} // Ω 	electrical resistance 	kg⋅m2⋅s−3⋅A−2 	V/A = J⋅s/C2
-	Siemens= Derived{-1,-2,3,2,0,0,0} // S 	electrical conductance 	kg−1⋅m−2⋅s3⋅A2 	Ω−1
-	Weber= Derived{1,2,-2,-1,0,0,0} // 	Wb 	magnetic flux 	kg⋅m2⋅s−2⋅A−1 	V⋅s
-	Tesla= Derived{1,0,-2,-1,0,0,0} // T 	magnetic flux density 	kg⋅s−2⋅A−1 	Wb/m2
-	Henry= Derived{1,2,-2,-2,0,0,0} // H 	inductance 	kg⋅m2⋅s−2⋅A−2 	Wb/A
+	Distance=Derived(Length)
+	Duration=Derived(Time)
+	Frequency =	Duration.Reciprocal()
+	Current = Derived(Ampere)
+	Rate= Frequency
+	Area =	DerivedsCombine(Distance,Distance)
+	Volume =	DerivedsCombine(Distance,Distance,Distance)
+	Velocity =	DerivedsCombine(Distance,Rate)
+	Acceleration =	DerivedsCombine(Velocity,Rate)
+	Density =DerivedsCombine(Derived(Mass),Volume.Reciprocal())
+	Energy =DerivedsCombine(Derived(Mass),Velocity,Velocity)
+	Force = DerivedsCombine(Derived(Mass),Acceleration)
+	Power = DerivedsCombine(Energy,Rate)
+	Pressure = DerivedsCombine(Force,Area.Reciprocal())
+	Coulomb = DerivedsCombine(Current,Duration)
+	Volt= DerivedsCombine(Power,Current.Reciprocal()) // Derived{M:1,L:2,T:-3,a:-1} // 	V 	electric potential difference[a] 	kg⋅m2⋅s−3⋅A−1 	W/A = J/C
+	Farad= DerivedsCombine(Coulomb,Volt.Reciprocal()) // Derived{M:1,LDerived{M:-1,L:-2,T:4,a:2} // F 	capacitance 	kg−1⋅m−2⋅s4⋅A2 	C/V = C2/J
+	Ohm= DerivedsCombine(Volt,Current.Reciprocal()) // Derived{M:1,L Derived{M:1,L:2,T:-3,a:-2} // Ω 	electrical resistance 	kg⋅m2⋅s−3⋅A−2 	V/A = J⋅s/C2
+	Siemens= Ohm.Reciprocal() // S 	electrical conductance 	kg−1⋅m−2⋅s3⋅A2 	Ω−1
+	Weber= DerivedsCombine(Volt,Duration) // Derived{M:1,L:2,T:-2,a:-1} // 	Wb 	magnetic flux 	kg⋅m2⋅s−2⋅A−1 	V⋅s
+	Tesla= DerivedsCombine(Weber,Area.Reciprocal()) // Derived{M:1,T:-2,a:-1} // T 	magnetic flux density 	kg⋅s−2⋅A−1 	Wb/m2
+	Henry= DerivedsCombine(Weber,Current.Reciprocal()) // Derived{M:1,L:2,T:-2,a:-2} // H 	inductance 	kg⋅m2⋅s−2⋅A−2 	Wb/A
 //	Celsius= Derived{0,0,0,0,1,0,0} // °C 	Celsius temperature 	K 	273.15
-	Becquerel= Derived{0,0,-1,0,0,0,0} // Bq 	activity referred to a radionuclide 	s−1 	
-	Ggray= Derived{0,2,-2,0,0,0,0} // Gy 	absorbed dose, kerma 	m2⋅s−2 	J/kg
-	Sievert= Derived{0,2,-2,0,0,0,0} // Sv 	dose equivalent 	m2⋅s−2
+	Becquerel= Rate // Bq 	activity referred to a radionuclide 	s−1
+	Gray= DerivedsCombine(Energy,Derived(Mass).Reciprocal()) // Derived{L:2,T:-2} // Gy 	absorbed dose, kerma 	m2⋅s−2 	J/kg
+	Sievert= Gray // Sv 	dose equivalent 	m2⋅s−2
 )
 
-type Sr Derived
+type Scaled struct{Derived;Constant}
 
 // light
 var (
-	Lumen= Sr{0,0,0,0,0,0,1} // lm 	luminous flux 	cd⋅sr[nc 2] 	cd⋅sr
-	Lux= Sr{0,-2,0,0,0,0,1} // lx 	illuminance 	cd⋅sr⋅m−2[nc 2] 	lm/m2
+	Lumen= Scaled{Derived(Candela),steradian} // lm 	luminous flux 	cd⋅sr[nc 2] 	cd⋅sr
+	Lux= Scaled{DerivedsCombine(Derived(Candela),Area.Reciprocal()),steradian} // lx 	illuminance 	cd⋅sr⋅m−2[nc 2] 	lm/m2
 )
+//// 
 
+type Constant float64
 
-//// constsnats
-//const (
-//	radian[nc 1] 	rad 	plane angle 		1  2pi
-//	steradian[nc 1] 	sr 	solid angle 		1  4pi
-//	SpeedOfLight c	{0,1,-2} 299792458 m/s
-//	PlanksContant h {1,2,-1} 6.62607015×10−34
-//	BoltzmannConstant 1.380649×10−23 J⋅K−1	
-//	Avagadros 6.02214076×1023 mol−1 
-//	ElementaryCharge e 	1.602176634×10−19 
-//)
+const (
+	radian Constant = 2*math.Pi // r
+	steradian = 4*math.Pi // Sr
+	SpeedOfLight = 299792458 // c
+	PlanksContant = 6.62607015e-34 //h {1,2,-1}
+	BoltzmannConstant =1.380649e-23 // b J⋅K−1	
+	Avagadros = 6.02214076e23 // mol−1 
+	ElementaryCharge = 1.602176634e-19 // e
+)
 
 //func Combine[Ds []D, D ~Dims](ds Ds)(cd Dims){
 //	for _,d:=range ds{
@@ -75,17 +87,20 @@ var (
 
 type Derived Dims
 
-func (d Derived) Reciprical()Derived{
-	return Derived(Dims(d).Reciprical())
-}
-
 func DerivedsCombine(ds ...Derived)(cd Derived){
 	for _,d:=range ds{
 		cd.M+=d.M
 		cd.L+=d.L
 		cd.T+=d.T
+		cd.a+=d.a
+		cd.k+=d.k
+		cd.m+=d.m
+		cd.c+=d.c
 	}
 	return
+}
+func (d Derived) Reciprocal() Derived{
+	return Derived{-d.M,-d.L,-d.T,-d.a,-d.k,-d.m,-d.c}
 }
 
 func (d Derived) String()string{
@@ -143,10 +158,6 @@ func DimsCombine(ds ...Dims)(cd Dims){
 	return
 }
 
-func (d Dims) Reciprical()Dims{
-	return Dims{-d.M,-d.L,-d.T}
-}
-
 func (d Dims) String()string{
 	return strings.Join(
 		slices.DeleteFunc(
@@ -154,6 +165,10 @@ func (d Dims) String()string{
 				DimString(d.M,"kg"),
 				DimString(d.L,"m"),
 				DimString(d.T,"s"),
+				DimString(d.a,"A"),
+				DimString(d.k,"K"),
+				DimString(d.m,"mol"),
+				DimString(d.c,"Cd"),
 			},
 			func(s string)bool{return s==""},
 		),
--- units_test.go
+++ units_test.go
@@ -3,20 +3,20 @@ package scalingSuffix
 import "fmt"
 
 func ExampleDims(){
-	fmt.Println(Dims{0,0,1})
-	fmt.Println(Dims{1,1,1})
-	fmt.Println(Dims{-1,-1,-1})
-	fmt.Println(Dims{2,0,1})
-	fmt.Println(Dims{0,1,-2})
+	fmt.Println(Time)
+	fmt.Println(Dims{M:1,L:1,T:1})
+	fmt.Println(Dims{M:-1,L:-1,T:-1})
+	fmt.Println(Dims{M:2,T:-1})
+	fmt.Println(Dims{L:1,T:-2})
 	fmt.Println(Energy)
 	fmt.Println(Dims(Energy))
-	fmt.Println(Derived(Time.Reciprical()))
-	fmt.Println(DerivedsCombine(Energy,Derived(Time.Reciprical())))
+	fmt.Println(Derived{T:-1})
+	fmt.Println(DerivedsCombine(Energy,Frequency))
 	// Output:
 	// s
 	// kg⋅m⋅s
 	// kg-1⋅m-1⋅s-1
-	// kg2⋅s
+	// kg2⋅s-1
 	// m⋅s-2
 	// J
 	// kg⋅m2⋅s-2
