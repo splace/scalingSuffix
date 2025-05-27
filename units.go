2dbe75e5df7ea1eb6c2cde3c746a990651fd22f5
splace <psiplace@netscape.net>
2025-05-28 00:58:53 +0100

    units def in field tags .1

--- units.go
+++ units.go
@@ -5,7 +5,10 @@ import "fmt"
 import "math"
 import "strconv"
 import "strings"
-import "slices"
+//import "slices"
+import "reflect"
+import "iter"
+//import "log"
 
 
 // SIUnits's are SI's with metric units suffixes added.
@@ -15,53 +18,65 @@ func NewSIUnits[N Number](v N, f fmt.Stringer) SIUnits[N]{
 	return SIUnits[N]{SI[N]{v},f}
 }
 
-var (
-	Mass = Dims{M:1}
-	Length = Dims{L:1}
-	Time = Dims{T:1}
-	Ampere  = Dims{a:1}
-	Kelvin  = Dims{k:1}
-	Mole 	 = Dims{m:1}
-	Candela = Dims{c:1}
-)
+//var (
+//	Mass = Dims{0,1,0,0,0,0,0}
+//	Length = Dims{L:1}
+//	Time = Dims{T:1}
+//	Ampere  = Dims{I:1}
+//	Kelvin  = Dims{k:1}
+//	Mole 	 = Dims{m:1}
+//	Candela = Dims{c:1}
+//)
 
 var (
-	Distance=Derived(Length)
-	Duration=Derived(Time)
-	Frequency =	Duration.Reciprocal()
-	Current = Derived(Ampere)
-	Rate= Frequency
-	Area =	DerivedsCombine(Distance,Distance)
-	Volume =	DerivedsCombine(Distance,Distance,Distance)
-	Velocity =	DerivedsCombine(Distance,Rate)
-	Acceleration =	DerivedsCombine(Velocity,Rate)
-	Density =DerivedsCombine(Derived(Mass),Volume.Reciprocal())
-	Energy =DerivedsCombine(Derived(Mass),Velocity,Velocity)
-	Force = DerivedsCombine(Derived(Mass),Acceleration)
-	Power = DerivedsCombine(Energy,Rate)
-	Pressure = DerivedsCombine(Force,Area.Reciprocal())
-	Coulomb = DerivedsCombine(Current,Duration)
-	Volt= DerivedsCombine(Power,Current.Reciprocal()) // Derived{M:1,L:2,T:-3,a:-1} // 	V 	electric potential difference[a] 	kg⋅m2⋅s−3⋅A−1 	W/A = J/C
-	Farad= DerivedsCombine(Coulomb,Volt.Reciprocal()) // Derived{M:1,LDerived{M:-1,L:-2,T:4,a:2} // F 	capacitance 	kg−1⋅m−2⋅s4⋅A2 	C/V = C2/J
-	Ohm= DerivedsCombine(Volt,Current.Reciprocal()) // Derived{M:1,L Derived{M:1,L:2,T:-3,a:-2} // Ω 	electrical resistance 	kg⋅m2⋅s−3⋅A−2 	V/A = J⋅s/C2
-	Siemens= Ohm.Reciprocal() // S 	electrical conductance 	kg−1⋅m−2⋅s3⋅A2 	Ω−1
-	Weber= DerivedsCombine(Volt,Duration) // Derived{M:1,L:2,T:-2,a:-1} // 	Wb 	magnetic flux 	kg⋅m2⋅s−2⋅A−1 	V⋅s
-	Tesla= DerivedsCombine(Weber,Area.Reciprocal()) // Derived{M:1,T:-2,a:-1} // T 	magnetic flux density 	kg⋅s−2⋅A−1 	Wb/m2
-	Henry= DerivedsCombine(Weber,Current.Reciprocal()) // Derived{M:1,L:2,T:-2,a:-2} // H 	inductance 	kg⋅m2⋅s−2⋅A−2 	Wb/A
-//	Celsius= Derived{0,0,0,0,1,0,0} // °C 	Celsius temperature 	K 	273.15
-	Becquerel= Rate // Bq 	activity referred to a radionuclide 	s−1
-	Gray= DerivedsCombine(Energy,Derived(Mass).Reciprocal()) // Derived{L:2,T:-2} // Gy 	absorbed dose, kerma 	m2⋅s−2 	J/kg
-	Sievert= Gray // Sv 	dose equivalent 	m2⋅s−2
-)
+	Distance=Derived(sSI{Length:1})
+	Duration=Derived(sSI{Mass:1})
+//	Frequency =	Duration.Reciprocal()
+	Current = Derived(sSI{Ampere:1})
+//	Rate= Frequency
+//	Area =	DerivedsCombine(Distance,Distance)
+//	Volume =	DerivedsCombine(Distance,Distance,Distance)
+//	Velocity =	DerivedsCombine(Distance,Rate)
+//	Acceleration =	DerivedsCombine(Velocity,Rate)
+//	Density =DerivedsCombine(Derived{1,0,0,0,0,0,0},Volume.Reciprocal())
+//	Energy =DerivedsCombine(Derived{1,0,0,0,0,0,0},Velocity,Velocity)
+//	Force = DerivedsCombine(Derived{1,0,0,0,0,0,0},Acceleration)
+//	Power = DerivedsCombine(Energy,Rate)
+//	Pressure = DerivedsCombine(Force,Area.Reciprocal())
+//	Charge = DerivedsCombine(Current,Duration)
+//	Temperature	=Derived{0,0,0,0,1,0,0}
+//	Resistivity = DerivedsCombine(Ohm,Distance)
+//	Conductivity = Resistivity.Reciprocal()
+// 	HeatCapacity = DerivedsCombine(Energy,Kelvin.Reciprocal())
+// 	VolumetricHeatCapacity = DerivedsCombine(HeatCapacity,Volume.Reciprocal())
+// 	MolarHeatCapacity = DerivedsCombine(HeatCapacity,Derived{0,0,0,0,0,1,0}.Reciprocal())
 
-type Scaled struct{Derived;Constant}
+)
+////
+//var (
+//	Coulomb = Charge
+//	Volt= DerivedsCombine(Power,Current.Reciprocal())
+//	Farad= DerivedsCombine(Coulomb,Volt.Reciprocal())
+//	Ohm= DerivedsCombine(Volt,Current.Reciprocal())
+//	Siemens= Ohm.Reciprocal()
+//	Weber= DerivedsCombine(Volt,Duration) 
+//	Tesla= DerivedsCombine(Weber,Area.Reciprocal())
+//	Henry= DerivedsCombine(Weber,Current.Reciprocal())
+////	Celsius= Derived{0,0,0,0,1,0,0} // °C 	Celsius temperature 	K 	273.15
+//	Becquerel= Rate 
+//	Gray= DerivedsCombine(Energy,Derived{1,0,0,0,0,0,0}.Reciprocal())
+//	Sievert= Gray
+//	
+//)
+
+type Modified struct{Derived;Constant}
 
 // light
-var (
-	Lumen= Scaled{Derived(Candela),steradian} // lm 	luminous flux 	cd⋅sr[nc 2] 	cd⋅sr
-	Lux= Scaled{DerivedsCombine(Derived(Candela),Area.Reciprocal()),steradian} // lx 	illuminance 	cd⋅sr⋅m−2[nc 2] 	lm/m2
-)
-//// 
+//var (
+//	Lumen= Modified{Derived{0,0,0,0,0,0,1},steradian}
+//	Lux= Modified{DerivedsCombine(Derived{0,0,0,0,0,0,1},Area.Reciprocal()),steradian}
+//)
+////// 
 
 type Constant float64
 
@@ -71,70 +86,62 @@ const (
 	SpeedOfLight = 299792458 // c
 	PlanksContant = 6.62607015e-34 //h {1,2,-1}
 	BoltzmannConstant =1.380649e-23 // b J⋅K−1	
-	Avagadros = 6.02214076e23 // mol−1 
+	Avagadros = 6.02214076e23 // NA mol−1 
 	ElementaryCharge = 1.602176634e-19 // e
 )
 
-//func Combine[Ds []D, D ~Dims](ds Ds)(cd Dims){
+
+type Derived sSI
+
+//func DerivedsCombine(ds ...Derived)(cd Derived){
 //	for _,d:=range ds{
 //		cd.M+=d.M
 //		cd.L+=d.L
 //		cd.T+=d.T
+//		cd.I+=d.I
+//		cd.k+=d.k
+//		cd.m+=d.m
+//		cd.c+=d.c
 //	}
 //	return
 //}
 
+//func (d Derived) Reciprocal() Derived{
+//	return Derived{-d.M,-d.L,-d.T,-d.I,-d.k,-d.m,-d.c}
+//}
 
-type Derived Dims
-
-func DerivedsCombine(ds ...Derived)(cd Derived){
-	for _,d:=range ds{
-		cd.M+=d.M
-		cd.L+=d.L
-		cd.T+=d.T
-		cd.a+=d.a
-		cd.k+=d.k
-		cd.m+=d.m
-		cd.c+=d.c
-	}
-	return
-}
-func (d Derived) Reciprocal() Derived{
-	return Derived{-d.M,-d.L,-d.T,-d.a,-d.k,-d.m,-d.c}
-}
-
-func (d Derived) String()string{
-	switch d{
-	case Energy:
-		return "J"
-	case Force:
-		return "N"
-	case Power:
-		return "W"
-	case Pressure:
-		return "Pa"
-	case Frequency:
-		return "Hz"
-	case Coulomb:
-		return "C"
-	case Volt:
-		return "V"
-	case Farad:
-		return "F"
-	case Ohm:
-		return "Ω"
-	case Siemens:
-		return "S"
-	case Weber:
-		return "Wb"
-	case Tesla:
-		return "T"
-	case Henry:
-		return "H"
-	default:
-		return Dims(d).String()
-	}	
-}
+//func (d Derived) String()string{
+//	switch d{
+//	case Energy:
+//		return "J"
+//	case Force:
+//		return "N"
+//	case Power:
+//		return "W"
+//	case Pressure:
+//		return "Pa"
+//	case Frequency:
+//		return "Hz"
+//	case Coulomb:
+//		return "C"
+//	case Volt:
+//		return "V"
+//	case Farad:
+//		return "F"
+//	case Ohm:
+//		return "Ω"
+//	case Siemens:
+//		return "S"
+//	case Weber:
+//		return "Wb"
+//	case Tesla:
+//		return "T"
+//	case Henry:
+//		return "H"
+//	default:
+//		return Dims(d).String()
+//	}	
+//}
 
 func DimString(d int8, s string)(_ string){
 	switch d{
@@ -147,35 +154,143 @@ func DimString(d int8, s string)(_ string){
 	}
 }
 
-type Dims struct{M,L,T,a,k,m,c int8}
+var SuperReplacer=strings.NewReplacer(
+	"-","⁻",
+	"1","¹",
+	"2","²",
+	"3","³",
+	"4","⁴",
+	"5","⁵",
+	"6","⁶",
+	"7","⁷",
+	"8","⁸",
+)
 
-func DimsCombine(ds ...Dims)(cd Dims){
-	for _,d:=range ds{
-		cd.M+=d.M
-		cd.L+=d.L
-		cd.T+=d.T
+//¹ U+00B9
+//² U+00B2
+//³ U+00B3
+//⁴	U+2074	&#8308;	&#x2074;		SUPERSCRIPT FOUR
+//⁵	U+2075	&#8309;	&#x2075;		SUPERSCRIPT FIVE
+//⁶	U+2076	&#8310;	&#x2076;		SUPERSCRIPT SIX
+//⁷	U+2077	&#8311;	&#x2077;		SUPERSCRIPT SEVEN
+//⁸	U+2078	&#8312;	&#x2078;		SUPERSCRIPT EIGHT
+//⁺ U+207A
+//⁻ U+207B 
+
+
+type sSI struct{
+	Mass int8 `unit:"kg" dim:"M"`
+	Length int8 `unit:"m" dim:"L"`
+	Time int8 `unit:"s" dim:"T"`
+	Ampere int8 `unit:"A" dim:"I"`
+	Kelvin int8 `unit:"K" dim:"θ"`
+	Mole int8 `unit:"mol" dim:"N"`
+	Candela int8 `unit:"Cd" dim:"J"` 
+}
+
+type CGS struct{
+	Mass int8 `unit:"g" dim:"m" SI:"0.001"`
+	Length int8 `unit:"cm" dim:"L" SI:"0.01"`
+	Time int8 `unit:"s" dim:'t'`
+	Ampere int8 `unit:"A" dim:'I'`
+	Kelvin int8 `unit:"K" dim:"θ"`
+	Mole int8 `unit:"mol" dim:"N"`
+	Candela int8 `unit:"Cd" dim:"J"` 
+}
+
+type Imperial struct{
+	Mass int8 `unit:"lb" dim:"M" SI:"0.45359237"`
+	Length int8 `unit:"ft" dim:"L" SI:"0.3048"`
+	Time int8 `unit:"s" dim:"T"`
+}
+
+//var DimUnits= map[Dims]string{
+//	Mass:"kg",
+//	Length:"m",
+//	Time:"s",
+//	Ampere:"A",
+//	Kelvin:"K",
+//	Mole:"mol",
+//	Candela:"Cd",
+//}
+
+
+
+//"M"),
+//"L"),
+//"T"),
+//"I"),
+//"θ"),
+//"N"),
+//"J"),
+
+//var UnitDims=Pivot(DimUnits)
+
+
+
+//func (d Dims) String()string{
+//	return strings.Join(
+//		slices.DeleteFunc(
+//			[]string{
+//				DimString(d.M,"kg"),
+//				DimString(d.L,"m"),
+//				DimString(d.T,"s"),
+//				DimString(d.I,"A"),
+//				DimString(d.k,"K"),
+//				DimString(d.m,"mol"),
+//				DimString(d.c,"Cd"),
+//			},
+//			func(s string)bool{return s==""},
+//		),
+//		"⋅",
+//	)
+//}
+
+func Tags(t reflect.Type,l string) iter.Seq[string]{
+	if l==""|| l=="name" {
+		return func(yield func(string) bool) {
+			for _,f:=range reflect.VisibleFields(t){
+				if !yield(fmt.Sprint(f.Name)){
+					return
+				}
+			}
+		}
 	}
+	return func(yield func(string) bool) {
+		for _,f:=range reflect.VisibleFields(t){
+			if !yield(fmt.Sprint(f.Tag.Get(l))){
+				return
+			}
+		}
+	}
+}
+
+func (d *sSI) Scan(state fmt.ScanState,verb rune) (err error){
+	var s string
+	_,err=fmt.Fscan(state,&s)
+	
 	return
 }
 
-func (d Dims) String()string{
-	return strings.Join(
-		slices.DeleteFunc(
-			[]string{
-				DimString(d.M,"kg"),
-				DimString(d.L,"m"),
-				DimString(d.T,"s"),
-				DimString(d.a,"A"),
-				DimString(d.k,"K"),
-				DimString(d.m,"mol"),
-				DimString(d.c,"Cd"),
-			},
-			func(s string)bool{return s==""},
-		),
-		"⋅",
-	)
-}
+//type Dim Dims
 
+//func (d Dim) String()string{
+//	return strings.Join(
+//		slices.DeleteFunc(
+//			[]string{
+//				DimString(d.M,"M"),
+//				DimString(d.L,"L"),
+//				DimString(d.T,"T"),
+//				DimString(d.I,"I"),
+//				DimString(d.k,"θ"),
+//				DimString(d.m,"N"),
+//				DimString(d.c,"J"),
+//			},
+//			func(s string)bool{return s==""},
+//		),
+//		" ",
+//	)
+//}
 
 
 
--- units_test.go
+++ units_test.go
@@ -1,26 +1,37 @@
 package scalingSuffix
 
 import "fmt"
+import "slices"
+import "reflect"
 
-func ExampleDims(){
-	fmt.Println(Time)
-	fmt.Println(Dims{M:1,L:1,T:1})
-	fmt.Println(Dims{M:-1,L:-1,T:-1})
-	fmt.Println(Dims{M:2,T:-1})
-	fmt.Println(Dims{L:1,T:-2})
-	fmt.Println(Energy)
-	fmt.Println(Dims(Energy))
-	fmt.Println(Derived{T:-1})
-	fmt.Println(DerivedsCombine(Energy,Frequency))
+func ExamplesSI(){
+	fmt.Println(slices.Collect(Tags(reflect.TypeOf(sSI{}),"unit")))
+	fmt.Println(slices.Collect(Tags(reflect.TypeOf(Imperial{}),"SI")))
 	// Output:
 	// s
-	// kg⋅m⋅s
-	// kg-1⋅m-1⋅s-1
-	// kg2⋅s-1
-	// m⋅s-2
-	// J
-	// kg⋅m2⋅s-2
-	// Hz
-	// W
+
 }
+
+
+
+//func ExampleDims(){
+//	fmt.Println(Time)
+//	fmt.Println(Dims{M:1,L:1,T:1})
+//	fmt.Println(Dims{M:-1,L:-1,T:-1})
+//	fmt.Println(Dims{M:2,T:-1})
+//	fmt.Println(Dims{L:1,T:-2})
+//	fmt.Println(Energy,Dims(Energy),Dim(Energy))
+//	fmt.Println(Derived{T:-1})
+//	fmt.Println(DerivedsCombine(Energy,Frequency))
+//	// Output:
+//	// s
+//	// kg⋅m⋅s
+//	// kg-1⋅m-1⋅s-1
+//	// kg2⋅s-1
+//	// m⋅s-2
+//	// J
+//	// kg⋅m2⋅s-2
+//	// Hz
+//	// W
+//}
 
